46e76e073f49eee94d18f97b3b8476cd9b84f03b
Julio Rivas <rivasjulio@ymail.com>
2023-06-27 21:04:15 -0400

    completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 const init = () => {
-  
-}
+    const inputForm = document.querySelector("form");
+
+    inputForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        console.log(event);
+    });
+    }; 
+
 
 document.addEventListener('DOMContentLoaded', init);
+
+
+
+
+
+    
